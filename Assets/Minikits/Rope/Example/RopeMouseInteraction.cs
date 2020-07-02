c13f5e217e1ff3836a2decc8deca22df006359d4
Luis <luis.delgadillo@inmersys.com>
2020-07-01 23:10:56 -0500

    Se han agregado las fisicas para ambos plugs

---
+++ Assets/Minikits/Rope/Example/RopeMouseInteraction.cs
@@ -0,0 +1,97 @@
+﻿using UnityEngine;
+using Unity.Mathematics;
+
+namespace RopeMinikit
+{
+    public class RopeMouseInteraction : MonoBehaviour
+    {
+        public Mesh indicatorMesh;
+        public Material indicatorMaterial;
+
+        public Rope[] ropes;
+        
+        protected Rope pulledRope;
+        protected int pulledParticle;
+        protected float pulledDistance;
+        protected float3 currentPosition;
+        protected float3 targetPosition;
+        
+        public void FixedUpdate()
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+            if (Input.GetMouseButton(0))
+            {
+                // Mouse down
+                if (pulledRope == null)
+                {
+                    // Not pulling a rope, find the closest one to the mouse
+                    var closestRopeIndex = -1;
+                    var closestParticleIndex = -1;
+                    var closestDistance = 0.0f;
+                    var closestDistanceAlongRay = 0.0f;
+
+                    for (int i = 0; i < ropes.Length; i++)
+                    {
+                        ropes[i].GetClosestParticle(ray, out int particleIndex, out float distance, out float distanceAlongRay);
+
+                        if (distance < closestDistance || i == 0)
+                        {
+                            closestRopeIndex = i;
+                            closestParticleIndex = particleIndex;
+                            closestDistance = distance;
+                            closestDistanceAlongRay = distanceAlongRay;
+                        }
+                    }
+
+                    if (closestRopeIndex != -1 && closestParticleIndex != -1 && ropes[closestRopeIndex].GetMassMultiplierAt(closestParticleIndex) > 0.0f)
+                    {
+                        // Found a rope and particle on the rope, start pulling that particle!
+                        pulledRope = ropes[closestRopeIndex];
+                        pulledParticle = closestParticleIndex;
+                        pulledDistance = closestDistanceAlongRay;
+                    }
+                }
+            }
+            else
+            {
+                // Mouse up
+                if (pulledRope != null)
+                {
+                    // Stop pulling the rope
+                    pulledRope.SetMassMultiplierAt(pulledParticle, 1.0f);
+                    pulledRope = null;
+                }
+            }
+
+            if (pulledRope != null)
+            {
+                // We are pulling the rope
+
+                // Adjust the grab plane
+                pulledDistance += Input.mouseScrollDelta.y * 2.0f;
+
+                // Move the rope particle to the mouse position on the grab-plane
+                currentPosition = pulledRope.GetPositionAt(pulledParticle);
+                targetPosition = ray.GetPoint(pulledDistance);
+
+                pulledRope.SetPositionAt(pulledParticle, targetPosition);
+                pulledRope.SetVelocityAt(pulledParticle, float3.zero);
+                pulledRope.SetMassMultiplierAt(pulledParticle, 0.0f);
+            }
+        }
+
+        public void Update()
+        {
+            if (indicatorMesh == null || indicatorMaterial == null)
+            {
+                return;
+            }
+
+            if (pulledRope != null)
+            {
+                Graphics.DrawMesh(indicatorMesh, Matrix4x4.TRS(currentPosition, Quaternion.identity, Vector3.one * 0.25f), indicatorMaterial, 0);
+            }
+        }
+    }
+}
---
+++ Assets/Minikits/Rope/RopePin.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using Unity.Mathematics;
+
+namespace RopeMinikit
+{
+    public class RopePin : MonoBehaviour
+    {
+        public Rope rope;
+        [DisableInPlayMode, Range(0.0f, 1.0f)] public float ropeLocation = 0.0f;
+        public bool automaticallyFindRopeLocation = false;
+        public new Transform transform;
+        public Vector3 localPoint;
+
+        protected int particleIndex;
+
+        public void OnEnable()
+        {
+            if (rope == null || transform == null)
+            {
+                return;
+            }
+
+            if (automaticallyFindRopeLocation)
+            {
+                var point = transform.TransformPoint(localPoint);
+                rope.GetClosestParticle(point, out particleIndex, out float distance);
+                ropeLocation = rope.GetScalarDistanceAt(particleIndex);
+            }
+            else
+            {
+                var ropeDistance = ropeLocation * rope.measurements.realCurveLength;
+                particleIndex = rope.GetParticleIndexAt(ropeDistance);
+            }
+
+            rope.SetMassMultiplierAt(particleIndex, 0.0f);
+        }
+
+        public void FixedUpdate()
+        {
+            if (rope == null || transform == null)
+            {
+                return;
+            }
+
+            rope.SetPositionAt(particleIndex, transform.TransformPoint(localPoint));
+        }
+
+        public void OnDisable()
+        {
+            if (rope == null || transform == null)
+            {
+                return;
+            }
+
+            rope.SetMassMultiplierAt(particleIndex, 1.0f);
+        }
+
+#if UNITY_EDITOR
+        public void OnDrawGizmos()
+        {
+            if (Application.isPlaying)
+            {
+                return;
+            }
+            if (rope == null || transform == null || rope.spawnPoints.Count < 2)
+            {
+                return;
+            }
+
+            var point = transform.TransformPoint(localPoint);
+            Gizmos.color = new Color(0.69f, 0.0f, 1.0f);
+            Gizmos.DrawWireCube(point, Vector3.one * 0.05f);
+
+            if (!automaticallyFindRopeLocation)
+            {
+                var localToWorld = (float4x4)rope.transform.localToWorldMatrix;
+                var ropeLength = rope.spawnPoints.GetLengthOfCurve(ref localToWorld);
+                rope.spawnPoints.GetPointAlongCurve(ref localToWorld, ropeLength * ropeLocation, out float3 ropePoint);
+
+                Gizmos.DrawWireCube(ropePoint, Vector3.one * 0.05f);
+                Gizmos.DrawLine(ropePoint, point);
+            }
+        }
+#endif
+    }
+}
