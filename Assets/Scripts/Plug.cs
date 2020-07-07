41b8afc4976f0a34f049577a2d648a02e2f3c922
Luis <luis.delgadillo@inmersys.com>
2020-07-07 11:20:44 -0500

    Se ha trabajado en la base del proyecto y se integro el modelo de la fabrica.

---
+++ Assets/Scripts/Plug.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Plug : MonoBehaviour
+{
+    [Header("CONFIG")]
+    public float Something;
+
+    [Header("REFERENCE")]
+    public Transform trans_plugHose;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HoseEdge")){
+
+            foreach (var hand in Player.instance.hands){       
+                    hand.DetachObject(other.gameObject);
+            }
+
+            other.GetComponent<Rigidbody>().isKinematic = true;
+            other.transform.position = trans_plugHose.position;
+            other.transform.rotation = trans_plugHose.rotation;
+        } 
+    }
+
+}
